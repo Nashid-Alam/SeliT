4af965fdf89337cca5420c00b6ff29f974dd52b4
Nashid-Alam <nashid17@gmail.com>
2021-12-16 15:29:58 -0500

    all CRUD operations work for products

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,62 @@
-const Product = require('../models/product');
+const Product = require("../models/product")
 
 const createProduct = async (req, res) => {
-    try {
-        const product = await new Product(req.body)
-        await product.save()
-        return res.status(201).json({
-            product,
-        });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
+  try {
+    const product = await new Product(req.body)
+    await product.save()
+    return res.status(201).json(product)
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+    return res.status(200).json(products)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getProductbyId = async (req, res) => {
+  try {
+		console.log(req.params)
+		const id = req.params.id
+    const products = await Product.findById(id)
+    return res.status(200).json(products)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 
-const getAllProducts = async (req, res) => {
-    try {
-        const products= await Product.find()
-        return res.status(200).json({ products })
-    } catch (error) {
-        return res.status(500).send(error.message);
-    }
+const deleteProductbyId = async (req, res) => {
+  try {
+		console.log(req.params)
+		const id = req.params.id
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    return res.status(200).json("Product Deleted")
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updateProductbyId = async (req, res) => {
+  try {
+		console.log(req.params)
+		const id = req.params.id
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body)
+    return res.status(200).json("Product updated")
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
 }
 
 module.exports = {
-    createProduct,
-    getAllProducts
+  createProduct,
+  getAllProducts,
+	getProductbyId,
+	deleteProductbyId,
+	updateProductbyId
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-const { Router } = require('express');
-const router = Router();
+const { Router } = require("express")
+const router = Router()
 const controllers = require("../controllers/index")
 
-router.get('/', (req, res) => res.send('This is root!'))
-router.get('/products', controllers.getAllProducts)
-router.post('/products', controllers.createProduct)
-module.exports = router;
+router.get("/", (req, res) => res.send("This is root!"))
+router.get("/products", controllers.getAllProducts)
+router.post("/products", controllers.createProduct)
+router.get("/product/:id", controllers.getProductbyId)
+router.put("/product/:id", controllers.updateProductbyId)
+router.delete("/product/:id", controllers.deleteProductbyId)
+
+module.exports = router
