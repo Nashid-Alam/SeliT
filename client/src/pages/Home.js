624214cd59e41d3ce690f9a7c96ae6a99ddd3cb0
Nashid-Alam <nashid17@gmail.com>
2021-12-17 11:30:06 -0500

    enabled cors on server. Axios calls on react success.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import ProductCard from '../components/ProductCard'
+import React from "react"
+import ProductCard from "../components/ProductCard"
+import axios from "axios"
 
 function Home() {
+  const getAllProduct = async () => {
+    const response = await axios.get("http://localhost:3001/api/products")
+    console.log(response.data)
+  }
+  getAllProduct()
+
   const productName = "Bike"
   const productRating = 4
-  const productImage= "https://image.shutterstock.com/image-photo/modern-black-bike-jpg-260nw-34468921.jpg"
+  const productImage =
+    "https://image.shutterstock.com/image-photo/modern-black-bike-jpg-260nw-34468921.jpg"
+
   return (
     <div>
-      <ProductCard/>
+      <ProductCard
+        name={productName}
+        image={productImage}
+        rating={productRating}
+      />
     </div>
   )
 }
