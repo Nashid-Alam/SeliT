3edfb4eb3785a4b69323707dc5527cff2a79a4f0
Nashid-Alam <nashid17@gmail.com>
2021-12-22 12:35:17 -0500

    heroku adds

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const routes = require("./routes")
 const db = require("./db")
 const cors = require("cors")
+const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
@@ -14,6 +15,14 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
 app.use("/api", routes)
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 
