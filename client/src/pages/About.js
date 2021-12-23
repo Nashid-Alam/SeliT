ebb4adaf0b3f3fa89c88cf27c32a3bd2a4d07d5a
Nashid-Alam <nashid17@gmail.com>
2021-12-23 15:16:45 -0500

    took out code for heroku

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      about page is working
+      <h1 className="aboutMe">Sell8 is an app that will revolutionize the selling of lightly used everyday products. Our app has a seamless user interface that allows sellers to easily post and price products and allows buyers to buy products. We have re-invented a sophisticated rating algorithm to give our buyers the confidence they need to buy with ease.</h1>
     </div>
   )
 }
--- db/index.js
+++ db/index.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose')
-require('dotenv').config() // Add this line
+const mongoose = require("mongoose")
 
-let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/SeliT'
+let MONGODB_URI = "mongodb://127.0.0.1:27017/SeliT"
 
 mongoose
-  .connect(dbUrl)
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log("Successfully connected to MongoDB.")
   })
   .catch((e) => {
-    console.error('Connection error', e.message)
+    console.error("Connection error", e.message)
   })
-mongoose.set('debug', true)
+
 const db = mongoose.connection
 
 module.exports = db
